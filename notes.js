bf2d4f4341c9ba1adb6840b8b007a77f73305491
icv852 <icv852@gmail.com>
2020-06-09 01:06:04 +0800

    Delete getNotes()

--- notes.js
+++ notes.js
@@ -1,8 +1,6 @@
 const fs = require('fs')
 const chalk = require('chalk')
 
-const getNotes= () => 'Your notes...'
-
 const addNote = (title,body) => {
     const notes = loadNotes()
     const duplicateNote = notes.find((note) => note.title === title)
@@ -65,7 +63,6 @@ const loadNotes = () => {
 }
 
 module.exports = {
-    getNotes: getNotes,
     addNote: addNote,
     removeNote: removeNote,
     listNotes: listNotes,
